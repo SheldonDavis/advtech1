191751456b8d993b849ec68622eae282f2d63398
SheldonDavis <sheldon.davis1515@gmail.com>
2014-10-16 09:16:09 -0400

    finished demo

--- demo3/script.js
+++ demo3/script.js
@@ -1,13 +1,19 @@
 var username="Fred"
 
-document.onclick=
+document.onmousemove=
 	function(evt){
-	makeOneCircle();
+	makeOneCircle(evt);
 }
 	function makeOneCircle(evt){
 		evt = (evt || event)//makes all browsers understand event
 		//make a new circle
 		var newc = document.createElement('div');
-		document.appendChild(newc);
+			newc.style.left=evt.clientX+"px";
+			newc.style.top=evt.clientY+"px";
+
+
+		document.body.appendChild(newc);
+
+
 
 	}
